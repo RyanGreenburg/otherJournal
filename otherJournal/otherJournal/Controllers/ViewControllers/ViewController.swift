73570a98eba2e82548bda13d43745f9bd80356b2
RYAN GREENBURG <ryangreenburg@RYANs-MacBook-Pro-2.local>
2019-10-23 09:56:31 -0600

    Initial commit with folder structure.

---
+++ otherJournal/otherJournal/Controllers/ViewControllers/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  otherJournal
+//
+//  Created by RYAN GREENBURG on 10/23/19.
+//  Copyright © 2019 RYAN GREENBURG. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
